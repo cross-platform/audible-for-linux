4e424b2f88a1122f2dfdc726faf49c809547976a
Marcus Tomlinson <themarcustomlinson@gmail.com>
2020-08-06 12:12:20 +0100

    Support CA, IE, AU, NZ, JP, IT, & IN marketplaces

--- main.js
+++ main.js
@@ -2,9 +2,19 @@ const {app, BrowserWindow, Menu, shell} = require('electron')
 
 const appName = 'Audible'
 
-if (app.getLocaleCountryCode() == 'GB') {
+// https://audible.custhelp.com/app/answers/detail/a_id/7267/~/what-is-an-audible-marketplace-and-which-is-best-for-me%3F
+
+if (app.getLocaleCountryCode() == 'CA') {
+  appUrl = 'https://www.audible.ca/library'
+  signInUrl = 'https://www.audible.ca/sign-in'
+}
+else if (app.getLocaleCountryCode() == 'GB' || app.getLocaleCountryCode() == 'IE') {
   appUrl = 'https://www.audible.co.uk/library'
   signInUrl = 'https://www.audible.co.uk/sign-in'
+}
+else if (app.getLocaleCountryCode() == 'AU' || app.getLocaleCountryCode() == 'NZ') {
+  appUrl = 'https://www.audible.com.au/library'
+  signInUrl = 'https://www.audible.com.au/sign-in'
 }
 else if (app.getLocaleCountryCode() == 'FR') {
   appUrl = 'https://www.audible.fr/library'
@@ -13,6 +23,18 @@ else if (app.getLocaleCountryCode() == 'FR') {
 else if (app.getLocaleCountryCode() == 'DE') {
   appUrl = 'https://www.audible.de/library'
   signInUrl = 'https://www.audible.de/sign-in'
+}
+else if (app.getLocaleCountryCode() == 'JP') {
+  appUrl = 'https://www.audible.co.jp/library'
+  signInUrl = 'https://www.audible.co.jp/sign-in'
+}
+else if (app.getLocaleCountryCode() == 'IT') {
+  appUrl = 'https://www.audible.it/library'
+  signInUrl = 'https://www.audible.it/sign-in'
+}
+else if (app.getLocaleCountryCode() == 'IN') {
+  appUrl = 'https://www.audible.in/library'
+  signInUrl = 'https://www.audible.in/sign-in'
 }
 else {
   appUrl = 'https://www.audible.com/library'
